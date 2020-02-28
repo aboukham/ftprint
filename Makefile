5e2d00e7d52fbc5040d4a0bcf719b8e6749e9753
aboukham <aboukham@gmail.com>
2020-02-28 23:09:26 +0100

    tester

--- Makefile
+++ Makefile
@@ -1,42 +1,25 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: aaboukha <marvin@42.fr>                    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/01/30 15:11:48 by aaboukha          #+#    #+#              #
-#    Updated: 2020/01/30 17:55:38 by aaboukha         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+NAME = test_printf
 
-NAME	=	libftprintf.a
-SRC		=	filling_spaces.c ft_strchr.c      ft_percent.c\
-			ft_substr.c      ft_stars.c       ft_print.c\
-	 		ft_putchar.c     flag_minus.c     ft_p.c\
-	 		ft_strdup.c      ft_atoi.c        ft_printf.c\
-	  		ft_putstr.c      ft_strlen.c      ft_character.c\
-			ft_itoa.c        ft_itoa_precision.c   ft_x.c\
-			ft_uitoa.c       ft_uitoa_precision.c   ft_u.c\
-		    dtoex.c          ft_to_upper.c        ft_integer.c\
-		    ft_nb_digit.c    ft_string.c           dtoex_prec.c
-OBJ		=	$(SRC:.c=.o)
-F		=	-Wall -Wextra -Werror
-H		=	ft_printf.h
+SRC = main.c
+
+OBJ = $(SRC:.c=.o)
+
+CFLAGS += -Wextra -W -Werror -fno-builtin -Wno-format -Wno-format-extra-args
+LDFLAGS += -L. -lftprintf
+
+$(NAME):
+	gcc $(CFLAGS) -o system_printf main.c
+	gcc $(CFLAGS) -D_FT_USER__ -o user_printf main.c $(LDFLAGS)
+	./system_printf > SYSOUT
+	./user_printf > USEROUT
 
 all: $(NAME)
 
-$(NAME): $(OBJ)
-	ar rc $(NAME) $(OBJ)
-	ranlib $(NAME)
-
-$(OBJ): $(SRC) $(H)
-	cc $(F) -c $(SRC)
-
 clean:
-	rm -rf $(OBJ)
+	rm -rf main.o
 
 fclean: clean
-	rm -rf $(NAME)
+	rm -rf system_printf
+	rm -rf user_printf
 
-re: fclean all
+rebuild: distclean $(NAME)
--- main.c
+++ main.c
@@ -1,40 +1,68 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aaboukha <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/23 00:02:48 by aaboukha          #+#    #+#             */
-/*   Updated: 2020/02/24 00:12:53 by aaboukha         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "ft_printf.h"
 #include <stdio.h>
-#define d 45
-//#define f ("%*p\n", 3, NULL)
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int	ft_printf(const char *, ...);
+
+#ifdef _FT_USER__
+#	define PRINTF_MACRO ft_printf
+#else
+#	define PRINTF_MACRO printf
+#endif
+
+static unsigned int my_strlen(char *str)
+{
+	unsigned int i = 0;
+	while (str[i])
+		i++;
+	return i;
+}
+
+static void	print_test(char *str)
+{
+#ifdef _FT_USER__
+	write(1, str, my_strlen(str));
+	write(1, " : ", 3);
+#else
+	printf("%s : ", str);
+#endif
+}
+
+static void	my_put_nbr(int nb)
+{
+	int		div = 1;
+	char	c;
+	
+	while (nb / div >= 10 || nb / div <= -10)
+		div = div * 10;
+	if (nb < 0)
+		write(1, "-", 1);
+	while (div)
+	{
+		if (nb < 0)
+			c = ((nb / div) % 10) * -1 + '0';
+		else
+			c = ((nb / div) % 10) + '0';
+		write(1, &c, 1);
+		div = div / 10;
+	}
+}
+
+static void	print_ret(int ret)
+{
+#ifdef _FT_USER__
+	write(1, " ", 1);
+	my_put_nbr(ret);
+	write(1, "\n", 1);
+#else
+	printf(" %d\n", ret);
+#endif
+}
 
 int	main(void)
 {
-	char	*s;
-
-/*	i = 0;
-	printf("%d\n", ft_printf("hello %s\n ssss%%%d\n ++++ %010u\n aaaaaaa %-*.10X\n bbbb %%%%%7s\n", "aboukhame---------", 345, -1, 10,  476, "aziz"));
-	printf("%d\n", printf("hello %s\n ssss%%%d\n ++++ %010u\n aaaaaaa %-*.10X\n bbbb %%%%%7s\n", "aboukhame---------", 345, -1, 10, 476, "aziz"));
+	#include "tests.c"
 	return (0);
-
-*/
-	s = NULL;
-	int rt = 0;
-	int rtor = 0;
-
-	rt = ft_printf("Bla bla %10s\n",s);
-	rtor = printf("Bla bla %10s\n",s);
-
-	//char	c = '\0';
-	//write(1, &c, 1);
-	printf ("\naziz = %d\nrtpr = %d\n", rt , rtor);
-
-
 }
+
